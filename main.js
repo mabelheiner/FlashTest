1b35e1189d5b8492a4e28407986d62d9982b6fc4
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2024-04-05 10:39:37 -0600

    frontend for flashcard api

---
+++ main.js
@@ -0,0 +1,22 @@
+import './style.css'
+
+async function getTerms() {
+  try{
+  const response = await fetch('https://flashcards-w643.onrender.com/flashcards/')
+  if (response.ok){
+    const data = await response.json()
+    const terms = JSON.stringify(data)
+    const list = terms.split(',')
+    list.map((item) =>{
+      document.querySelector('#app').innerHTML +=
+        `<li>${item}}</li>`
+    })
+    
+  }}
+  catch (error) {
+    document.querySelector('#app').innerHTML = 
+    `<div>${error.message}</div>`
+  }
+}
+
+getTerms()
