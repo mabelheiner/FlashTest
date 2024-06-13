117a04d4b35120cb76936e839c4ce4071e2ea0c6
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2024-06-13 13:51:10 -0600

    fetches and displays the terms and definitions from the flashcard api

--- main.js
+++ main.js
@@ -2,17 +2,19 @@ import './style.css'
 
 async function getTerms() {
   try{
-  const response = await fetch('https:flashcards-w643.onrender.com/flashcards')
+  const response = await fetch('https://flashcards-w643.onrender.com/flashcards/')
   console.log("hello")
   if (response.ok){
     const data = await response.json()
     const terms = JSON.stringify(data)
-    const list = terms.split(',')
+    const list = JSON.parse(terms)
+    console.log('List', list)
+    console.log('Item', list[0])
     list.map((item) =>{
       document.querySelector('#app').innerHTML +=
-        `<li>${item}}</li>`
+        `<p>Term: ${item.term}</p>
+        <p>Definition: ${item.definition}</p>`
     })
-    
   }}
   catch (error) {
     document.querySelector('#app').innerHTML = 
