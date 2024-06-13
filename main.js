66b950c600b034a99823a663fdd5d0a725f0e404
mabelheiner <83719321+mabelheiner@users.noreply.github.com>
2024-06-13 13:47:30 -0600

    correctly fetches information from flashcard api

--- main.js
+++ main.js
@@ -3,15 +3,18 @@ import './style.css'
 async function getTerms() {
   try{
   const response = await fetch('https://flashcards-w643.onrender.com/flashcards/')
+  console.log("hello")
   if (response.ok){
     const data = await response.json()
     const terms = JSON.stringify(data)
-    const list = terms.split(',')
+    const list = JSON.parse(terms)
+    console.log('List', list)
+    console.log('Item', list[0])
     list.map((item) =>{
       document.querySelector('#app').innerHTML +=
-        `<li>${item}}</li>`
+        `<li>${item.term}</li>
+        <li>${item.definition}</li>`
     })
-    
   }}
   catch (error) {
     document.querySelector('#app').innerHTML = 
